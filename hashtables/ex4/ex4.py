7da713b81802ab02545d7fb291fdacf306cf1b36
Guillermo Alfaro <galfarotolon@gmail.com>
2020-08-07 12:34:21 -0500

    completed exercise 4, total of 3 exercises completed so MVP met

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -4,6 +4,25 @@ def has_negatives(a):
     """
     # Your code here
 
+    # create cache to store all values for later
+    cache = {}
+
+    # store only the values with absolutes
+    result = []
+
+    # for each value in the array
+    for val in a:
+        # use absolute to check value
+        if cache.get(abs(val)):
+            # add value to the result array if its absolute
+            result.append(abs(val))
+        else:
+            # if value is not there yet, add it to cache
+            # to use later and reduce runtime
+            cache[abs(val)] = val
+        
+      
+
     return result
 
 
